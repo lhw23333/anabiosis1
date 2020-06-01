7cbe6cefb9b7acd48614534ad0d65fc3f12807a8
lhw23333 <18966823551@163.com>
2020-06-01 09:50:23 +0800

    0.2版本
    dash 和 参数调节

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -8,10 +8,19 @@ public class PlayerController : MonoBehaviour
     
     [Header("移动参数")]
     private Rigidbody2D rb; 
-    private float xVelocity;
+    private float x;
+    private float y;
+    private float xRaw;
+    private float yRaw;
+
     public int speed;
-    public float slideSpeed;
-    public bool canMove;
+    public float slideSpeed;//下滑速度
+    public float dashSpeed = 20;
+
+    public bool canMove;//墙跳和冲刺期间不可移动
+    public bool isDashing;
+    public bool hasDashed;//限制二次冲锋
+    public bool wallJumped;
 
     [Header("跳跃参数")]
     public float yVelocity;
@@ -22,6 +31,7 @@ public class PlayerController : MonoBehaviour
     [Header("按键检测")]
     public bool jumpPress;
     public bool catchPress;
+    public bool dashPress;
 
     
 
@@ -30,10 +40,10 @@ public class PlayerController : MonoBehaviour
     public float footOffset;
     public float groundDistansce ;
 
+    private bool groundTouch;
 
-    
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -49,26 +59,42 @@ public class PlayerController : MonoBehaviour
         {
             jumpPress = true;
         }
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            dashPress = true;
+        }
         catchPress = Input.GetKey(KeyCode.J);
     }
 
     private void FixedUpdate()
     {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        xRaw = Input.GetAxisRaw("Horizontal");
+        yRaw = Input.GetAxisRaw("Vertical");
+     
         PhysicsCheck();
         GroundMovement();
         Airmovement();
         yVelocity = rb.velocity.y;
         jumpPress = false;
-       
-        //PoolManager.Instance.GetObj("Shadow");
-    }
+        dashPress = false;
+
+        
+
+    //PoolManager.Instance.GetObj("Shadow");
+}
+
+    
+
 
     void GroundMovement()
     {
+        if (isDashing)
+            return;
         if (!canMove)
             return;
-        xVelocity = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(speed * xVelocity, rb.velocity.y);
+        rb.velocity = new Vector2(speed * x, rb.velocity.y);
 
     }
 
@@ -82,7 +108,7 @@ public class PlayerController : MonoBehaviour
 
         if(coll.isOnWall && !coll.isOnGround)
         {
-            if(xVelocity!=0)
+            if(x!=0)
             {
                 if (Input.GetKey(KeyCode.J))
                 {
@@ -95,9 +121,7 @@ public class PlayerController : MonoBehaviour
 
                 }
                     
-            }
-            
-            
+            }           
         }
         if (coll.isOnWall && !coll.isOnGround && jumpPress)
         {
@@ -105,12 +129,49 @@ public class PlayerController : MonoBehaviour
             WallJump();
         }
          
+        if(dashPress && !hasDashed)
+        {               
+             if (xRaw != 0 || yRaw != 0)
+                Dash(xRaw, yRaw);           
+        }
         
         
        
     }
 
-    
+    void Dash(float x, float y)
+    {
+        hasDashed = true;
+        rb.velocity = Vector2.zero;
+        Vector2 dir = new Vector2(x, y);
+        rb.velocity += dir.normalized * dashSpeed;
+
+        StartCoroutine(DashWait());
+    }
+
+    IEnumerator DashWait()//设置冲刺重力
+    {
+        StartCoroutine(GroundDash());
+
+        rb.gravityScale = 0;
+        GetComponent<BetterJumping>().enabled = false;
+        wallJumped = true;
+        isDashing = true;
+        yield return new WaitForSeconds(.1f);
+
+        //set gravity
+        rb.gravityScale = 5;
+        GetComponent<BetterJumping>().enabled = true;
+        wallJumped = false;
+        isDashing = false;
+    }
+
+    IEnumerator GroundDash()//地面多次冲刺
+    {
+        yield return new WaitForSeconds(.15f);
+        if (coll.isOnGround)
+            hasDashed = false;
+    }
 
     void WallJump()
     {
@@ -119,6 +180,7 @@ public class PlayerController : MonoBehaviour
         Vector2 wallDir = coll.isOnRightWall ? Vector2.left : Vector2.right;
 
         Jump(Vector2.up / 1.5f + wallDir / 1.5f);
+        wallJumped = true;
     }
 
     void WallSide()
@@ -183,17 +245,38 @@ public class PlayerController : MonoBehaviour
     }
 
     void PhysicsCheck()
-    {       
-     
+    {
+        if (coll.isOnGround && !groundTouch)
+        {
+            GroundTouch();
+            groundTouch = true;
+        }
+
+        if (!coll.isOnGround && groundTouch)
+        {
+            groundTouch = false;
+        }
+
+        if(coll.isOnGround && !isDashing)
+        {
+            wallJumped = false;
+            GetComponent<BetterJumping>().enabled = true;
+        }
+    }
+
+    void GroundTouch()
+    {
+        hasDashed = false;
+        isDashing = false;
     }
 
     void FlipDirction()
     {
-        if (xVelocity < 0)
+        if (x < 0)
         {
             transform.localScale = new Vector2(-30, 30);
         }
-        else if (xVelocity > 0)
+        else if (x > 0)
         {
             transform.localScale = new Vector2(30, 30);
         }
